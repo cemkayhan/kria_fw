b1d4583b3e7c91fa6d8c98392e4a0504844cd9c4
Cem Kayhan <cemkayhan42@gmail.com>
2023-09-07 12:18:11 +0000

    first version

--- src/bd/design_1/hdl/design_1_wrapper.v
+++ src/bd/design_1/hdl/design_1_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
-//Date        : Wed Aug 15 06:10:54 2018
-//Host        : serv.asis.local running 64-bit CentOS Linux release 7.9.2009 (Core)
+//Date        : Thu Sep  7 09:56:11 2023
+//Host        : serv running 64-bit Ubuntu 20.04.6 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
